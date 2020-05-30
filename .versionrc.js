bf2e1b166611dbf8b1aa046a40031717a9d09c28
fangbinwei <fangbinwei@yeah.net>
2020-05-30 18:21:41 +0800

    chore: standard version

---
+++ .versionrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  types: [
+    { type: 'feat', section: 'Features' },
+    { type: 'fix', section: 'Bug Fixes' },
+    { type: 'test', section: 'Tests', hidden: true },
+    { type: 'ci', hidden: true },
+    { type: 'chore', hidden: true },
+    { type: 'docs', hidden: true },
+    { type: 'style', hidden: true },
+    { type: 'refactor', hidden: true },
+    { type: 'perf', hidden: true }
+  ],
+  releaseCommitMessageFormat: 'chore(release): v{{currentTag}}'
+}
