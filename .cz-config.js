7e742042795eb59dbe68b71f20eaf17d5ef0a2e2
fangbinwei <fangbinwei@yeah.net>
2020-05-31 20:40:19 +0800

    chore: update cz-config

--- .cz-config.js
+++ .cz-config.js
@@ -67,7 +67,7 @@ module.exports = {
   allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix'],
   // skip any questions you want
-  skipQuestions: ['body'],
+  // skipQuestions: ['body'],
 
   // limit subject length
   subjectLimit: 100
