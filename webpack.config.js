2f9aaf2af8696b2fd77fa756b197b80e5d6199bd
fangbinwei <fangbinwei@yeah.net>
2020-06-03 03:19:09 +0800

    chore: bundle by webpack
    get rid of rollup!

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const webpack = require('webpack')
+const CopyPlugin = require('copy-webpack-plugin')
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin') // Wow!
+
+/**@type {import('webpack').Configuration}*/ const config = {
+  target: 'node',
+  node: {
+    __dirname: false
+  },
+
+  entry: './src/extension.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]' // means source code in dist/../[resource-path]
+  },
+  devtool: 'source-map',
+  externals: {
+    vscode: 'commonjs vscode'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'src/utils/clipboard', to: 'clipboard' }]
+    })
+  ]
+}
+
+module.exports = config
